86581333ad0c3899de94017574ebef83283fc4c6
Adam Wentz <adam@adamwentz.com>
2016-03-14 15:00:21 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='neural-image-analogies',
-    version='0.0.4',
+    version='0.0.5',
     description='Generate image analogies with a deep neural network.',
     author='Adam Wentz',
     author_email='adam@adamwentz.com',
