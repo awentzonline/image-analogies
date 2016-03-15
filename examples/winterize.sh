f3572def07f2156410c87bb36afd404ba812a5b7
Adam Wentz <adam@adamwentz.com>
2016-03-14 20:22:03 -0500

    season transfer example script

---
+++ examples/winterize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TARGET=$1
+OUTPUT_PREFIX=$2
+VGG_WEIGHTS=${3-vgg16_weights.h5}
+HEIGHT=${4-512}
+
+make_image_analogy.py \
+  images/season-xfer-A.jpg images/season-xfer-Ap.jpg \
+  $TARGET out/$OUTPUT_PREFIX-winterized/$OUTPUT_PREFIX-Bp \
+  --analogy-layers='conv1_1,conv2_1,conv3_1,conv4_1' \
+  --mrf-w=0.5 --scales=5 --contrast=0.2 \
+  --height=$HEIGHT \
+  --vgg-weights=$VGG_WEIGHTS --output-full
