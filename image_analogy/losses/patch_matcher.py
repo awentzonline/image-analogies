057b2424baa3ea8469322a367d46d9f4a70fe5df
Adam Wentz <adam.wentz@dollarshaveclub.com>
2016-12-03 17:58:55 -0800

    python3 print parens

--- image_analogy/losses/patch_matcher.py
+++ image_analogy/losses/patch_matcher.py
@@ -184,9 +184,9 @@ def congrid(a, newdims, method='linear', centre=False, minusone=False):
     old = np.array( a.shape )
     ndims = len( a.shape )
     if len( newdims ) != ndims:
-        print "[congrid] dimensions error. " \
+        print("[congrid] dimensions error. " \
               "This routine currently only support " \
-              "rebinning to the same number of dimensions."
+              "rebinning to the same number of dimensions.")
         return None
     newdims = np.asarray( newdims, dtype=float )
     dimlist = []
@@ -247,9 +247,9 @@ def congrid(a, newdims, method='linear', centre=False, minusone=False):
         newa = scipy.ndimage.map_coordinates(a, newcoords)
         return newa
     else:
-        print "Congrid error: Unrecognized interpolation type.\n", \
+        print("Congrid error: Unrecognized interpolation type.\n", \
               "Currently only \'neighbour\', \'nearest\',\'linear\',", \
-              "and \'spline\' are supported."
+              "and \'spline\' are supported.")
         return None
 
 
